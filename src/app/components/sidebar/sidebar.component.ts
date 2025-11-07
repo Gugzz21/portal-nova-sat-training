761a0a549adebf22770e2bb50970ce413a84c964
Gugzz21 <gustavodinizz966@gmail.com>
2025-11-07 09:30:06 -0300

    Melhora Sidebar Component

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-interface MenuItem {
-  icon: string;
-  label: string;
-  children?: MenuItem[];
-  isOpen?: boolean;
-}
-
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -15,41 +8,10 @@ interface MenuItem {
   imports: [CommonModule]
 })
 export class SidebarComponent {
-  @Input() isSidebarCollapsed = false;
-  @Output() sidebarToggle = new EventEmitter<void>();
 
-  menuItems: MenuItem[] = [
-    {
-      icon: 'fas fa-home',
-      label: 'Dashboard',
-      isOpen: false,
-      children: [
-        { icon: 'fas fa-chart-pie', label: 'Gráficos' },
-        { icon: 'fas fa-tasks', label: 'Projetos' },
-      ]
-    },
-    {
-      icon: 'fas fa-cog',
-      label: 'Configurações',
-      isOpen: false,
-      children: [
-        { icon: 'fas fa-user', label: 'Blablabla' },
-        { icon: 'fas fa-lock', label: 'Security' },
-      ]
-    },
-    {
-      icon: 'fas fa-envelope',
-      label: 'Mensagens'
-    }
-  ];
+  isOpened : boolean = true;
 
-  toggleSidebar() {
-    this.sidebarToggle.emit();
-  }
-
-  toggleMenuItem(item: MenuItem) {
-    if (!this.isSidebarCollapsed && item.children) {
-      item.isOpen = !item.isOpen;
-    }
+  toggleSidebar(): void {
+    this.isOpened = !this.isOpened;
   }
 }
